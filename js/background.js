eccb950c039cc26207b48d749294e5821a014946
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-01-10 23:21:09 -0200

    v0

--- js/background.js
+++ js/background.js
@@ -15,18 +15,18 @@ function changeImages(tabId) {
       console.log(message.title)
     });
 
-    var message = {
-      images: [
-      'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/005/093/3a2/2a09bd3.jpg',
-      'https://scontent.xx.fbcdn.net/v/t1.0-9/12227640_939751026119495_5550899219362026178_n.jpg?oh=5d962b39957017e87eca34dff169b259&oe=58EC570B',
-      'https://scontent.xx.fbcdn.net/v/t1.0-0/p206x206/14079615_1125800730789477_4512210792359351004_n.jpg?oh=4c7499bc6638d013229456120e02aa97&oe=58B8A074',
-      'https://scontent.xx.fbcdn.net/v/t1.0-9/67577_392099134218023_1411454898_n.jpg?oh=27d206293e49adc9a0e2de1da9b7e3cd&oe=58EB8391'
-      ]
-    };
+    chrome.storage.local.get('images', function(result) {
+      if (!result || !result.images || result.images.length == 0) return;
 
-    // Envia imagens para o content.js
-    chrome.tabs.sendMessage(tabId, message, function(request) {
-      console.log('Total de imagens substituidas: ' + request.totalImg);
+      var message = {
+        images: result.images
+      };
+
+      // Envia imagens para o content.js
+      chrome.tabs.sendMessage(tabId, message, function(request) {
+        console.log('Total de imagens substituidas: ' + request.totalImg);
+      });
+
     });
   });
 }
---
+++ popup/popup.js
@@ -0,0 +1,27 @@
+// When the popup HTML has loaded
+window.addEventListener('load', function(evt) {
+
+  chrome.storage.local.get('images', function (result) {
+    document.getElementById('images').innerHTML = result.images;
+  });
+
+  document.getElementById('form').addEventListener('submit', function() {
+    var images = document.getElementById('images').value;
+
+    var arrayImages = [];
+    if (images.indexOf(',') > 0) {
+      var allImages = images.split(',');
+      for (var i = 0; i < allImages.length; i++) {
+        if (!allImages[i]) continue;
+        arrayImages.push(allImages[i]);
+      }
+    } else {
+      arrayImages.push(images);
+    }
+
+    alert(images);
+
+    chrome.storage.local.set({ 'images': arrayImages });
+  });
+
+});
