d66a313434bbf017e6e9f951ba08f74e06968ca3
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-01-11 00:00:18 -0200

    Version like chrome extension

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
 
 gulp.task('zip', () => {
-    return gulp.src(['./*', '!dist', '!node_modules', '!package.json', '!gulpfile.js', 'screenshot.png', '!README.md'])
+    return gulp.src(['./*', '!dist', '!node_modules', '!package.json', '!gulpfile.js', '!screenshot.png', '!README.md'])
         .pipe(zip('troll-chrome-images.zip'))
         .pipe(gulp.dest('dist'));
 });
