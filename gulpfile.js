5d40a6edcb34062e3ee023c985b68d1ab6cf6014
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-04-02 22:32:31 -0300

    Fixing compress project bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,35 @@
-const gulp = require('gulp');
-const zip = require('gulp-zip');
+let gulp = require('gulp');
+let zip = require('gulp-zip');
+let copy = require('gulp-copy');
+let clean = require('gulp-clean');
+let gulpsync = require('gulp-sync')(gulp);
+
+let pjson = require('./package.json');
+
+gulp.task('build', gulpsync.sync(['clean:dist', 'clean:tmp', 'sources', 'zip', 'clean:tmp']));
+
+gulp.task('sources', () => {
+    let sources = ['manifest.json', 'assets/**/**', 'js/**/**', 'popup/**/**'];
+    return gulp
+        .src(sources)
+        .pipe(copy('.tmp'));
+});
 
 gulp.task('zip', () => {
-    return gulp.src(['./*', '!dist', '!node_modules', '!package.json', '!gulpfile.js', '!screenshot.png', '!README.md'])
-        .pipe(zip('troll-chrome-images.zip'))
+    return gulp
+        .src('.tmp/**')
+        .pipe(zip('troll-chrome-images-' + pjson.version + '.zip'))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean:tmp', () => {
+    return gulp
+        .src('.tmp', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('clean:dist', () => {
+    return gulp
+        .src('dist', { read: false })
+        .pipe(clean());
 });
