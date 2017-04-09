3209f5e265032df6f57b5fdf6b2e6328fde20143
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-04-09 12:25:05 -0300

    0.5 version

--- js/content.js
+++ js/content.js
@@ -1,25 +1,33 @@
+var tabTrolled = false;
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request == true)
+  if (!request) return;
+
+  tabTrolled = true;
+  changeImages();
+  setInterval(() => {
     changeImages();
+  }, 3000);
 });
 
 function changeImages() {
   chrome.storage.local.get('images', (result) => {
     if (!result || !result.images) return;
+
     let images = result.images;
-    let allImg = document.getElementsByTagName("img"), i = 0, img;
+    let imgTags = document.getElementsByTagName("img");
 
-    while (img = allImg[i++]) {
-      let style = img.style;
-
-      let height = style.height;
-      let width = style.width;
+    for (let i = 0; i < imgTags.length; i++) {
+      let img = imgTags[i];
+      if (contains(images, img.src)) continue;
 
       let newImage = randomImage(images);
       if (!newImage) return;
 
-      img.src = newImage;
+      let style = img.style;
+      let height = style.height;
+      let width = style.width;
 
+      img.src = newImage;
       img.style.maxHeight = '100%';
       img.style.maxWidth = '100%';
     }
@@ -29,3 +37,10 @@ function changeImages() {
 function randomImage(images) {
   return images[Math.floor((Math.random() * images.length))];
 }
+
+function contains(arr, img) {
+  var proxy = new Set(arr);
+  if (proxy.has(img)) return true;
+  else return false;
+}
+
